f530a7ee06f56dfa2013cf55d566b547eaf87a04
Parnets13 <livings3602@gmail.com>
2025-10-04 15:25:04 +0530

    best

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,6 +1,26 @@
+// import express from 'express';
+// import { createEvent, getAllEvents, getEventById, deleteEvent,updateEvent } from '../controllers/eventController.js';
+// // import { eventUpload } from '../middleware/media.js';
+// import multer from 'multer';
+
+// const upload = multer({ storage: multer.memoryStorage() });
+
+// export const eventUpload = upload.fields([
+//   { name: 'images', maxCount: 10 },
+//   { name: 'videos', maxCount: 5 }
+// ]);
+
+// const router = express.Router();
+
+// router.post('/', eventUpload, createEvent);
+// router.get('/', getAllEvents);
+// router.get('/:id', getEventById);
+// router.delete('/:id', deleteEvent);
+// router.put('/:id', eventUpload, updateEvent); 
+
+// export default router;
 import express from 'express';
-import { createEvent, getAllEvents, getEventById, deleteEvent,updateEvent } from '../controllers/eventController.js';
-// import { eventUpload } from '../middleware/media.js';
+import { createEvent, getAllEvents, getEventById, deleteEvent, updateEvent } from '../controllers/eventController.js';
 import multer from 'multer';
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -16,6 +36,6 @@ router.post('/', eventUpload, createEvent);
 router.get('/', getAllEvents);
 router.get('/:id', getEventById);
 router.delete('/:id', deleteEvent);
-router.put('/:id', eventUpload, updateEvent); 
+router.put('/:id', eventUpload, updateEvent);
 
 export default router;
